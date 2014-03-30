2b1e866440aa8baf45c0a0bdf81bf946621d79ad
Shishir Kumar Prasad <skprasad@cs.wisc.edu>
2014-03-30 16:22:08 -0700

    [WIP] Simplified to one public REST API for searching items in a store ..

--- web/StoreAssistBackend/app/controllers/SearchController.java
+++ web/StoreAssistBackend/app/controllers/SearchController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import com.google.common.base.Strings;
 
 import models.ItemLocation;
+import models.Store;
 import models.service.SearchService;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -16,18 +22,39 @@ import play.mvc.Result;
  */
 public class SearchController extends Controller {
 	
+	private static Logger LOG = Logger.getAnonymousLogger();
+	
+	private enum ErrorCode
+	{
+		MISSING_ITEM_IN_STORE("Item is not present in store database"),
+		UNAVAILABLE_STORE("Store not present in database");
+		
+		private String errorMsg;
+		
+		private ErrorCode(String errorMsg)
+		{
+			this.errorMsg = errorMsg;
+		}
+	}
+	
 	public static Result index()
 	{
 		return ok("Welcome to StoreAssist ..");
 	}
 	
 	/**
-	 * Returns the location of an item within a store.
+	 * Returns the location of items within a store.
 	 */
 	public static Result findItemLocation(String storeIdentifier, String item)
 	{
-		List<ItemLocation> itemLocations = SearchService.searchItemLocation(storeIdentifier, item);
-		return ok(Json.toJson(itemLocations));
+		if(Strings.isNullOrEmpty(item) || item.trim().length() < 2) {
+			LOG.severe("Please specify item to search.");
+			return ok(Json.toJson("MISSING MANDATORY ITEM PARAMETER"));
+		}
+		
+		List<String> items = Arrays.asList(item.split(","));
+		Map<String, List<ItemLocation>> itemsLocations = SearchService.searchItemsLocations(storeIdentifier, items);
+		return ok(Json.toJson(itemsLocations));
 	}
 	
 	/**
@@ -35,6 +62,7 @@ public class SearchController extends Controller {
 	 */
 	public static Result findStore(String storeIdentifier)
 	{
-		return ok();
+		Store store = SearchService.searchStore(storeIdentifier);
+		return ok(Json.toJson(store));
 	}
 }
--- web/StoreAssistBackend/app/models/service/SearchService.java
+++ web/StoreAssistBackend/app/models/service/SearchService.java
@@ -1,16 +1,68 @@
 package models.service;
 
 import java.util.List;
-
-import com.google.common.collect.Lists;
+import java.util.Map;
 
 import models.ItemLocation;
+import models.Store;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+/**
+ * A service API that searches entities related to stores - item locations, store locations etc.
+ * @author excelsior
+ *
+ */
 public class SearchService {
 
-	public static List<ItemLocation> searchItemLocation(String storeIdentifier, String item)
+	private enum StoreIdentifier
+	{
+		STORE_NAME, // Name of the store like Madison Fresh
+		ADDRESS,	// Address of the store like 2110 University Avenue
+		ZIP_CODE,	// Zip Code of store like 53726
+		EXTERNAL_STORE_ID,	// Any external known unique store id
+		INTERNAL_STORE_ID	// Internal unique store id
+	}
+	
+	/**
+	 * Searches all locations of items within a store.
+	 * 
+	 * @param storeIdentifier	- Identifier for store - store name/address/zip/store id
+	 * @param items				- List of items to be searched.
+	 * @return List of all locations for items in the specified store.
+	 */
+	public static Map<String, List<ItemLocation>> searchItemsLocations(String storeIdentifier, List<String> items)
+	{
+		Map<String, List<ItemLocation>> itemsLocations = Maps.newHashMap();
+		for(String item : items) {
+			itemsLocations.put(item, searchItemLocations(storeIdentifier, item));
+		}
+		
+		return itemsLocations;
+	}
+	
+	
+	/**
+	 * Search all locations of  an item in a store.
+	 * 
+	 * @param storeIdentifier	- Identifier for store - store name/address/zip/store id
+	 * @param item				- Item name.
+	 * @return	List of all locations for an item in the specified store.
+	 */
+	private static List<ItemLocation> searchItemLocations(String storeIdentifier, String item)
 	{
 		return getDummyLocation();
+	}
+	
+	/**
+	 * 
+	 * @param storeIdentifier
+	 * @return
+	 */
+	public static Store searchStore(String storeIdentifier)
+	{
+		return null;
 	}
 	
 	private static List<ItemLocation> getDummyLocation()
