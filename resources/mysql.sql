e0934134c7cbfab81e6030b18feacc99e609de54
Prakhar Panwaria <prakhar.panwaria9@gmail.com>
2014-03-31 01:10:48 -0500

    Added a row of item.

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -1,109 +1,175 @@
-use storeassist;
+-- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: storeassist
+-- ------------------------------------------------------
+-- Server version	5.5.35-0ubuntu0.12.10.1
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `aisle_shelf`
+--
 
-create table item_location
+DROP TABLE IF EXISTS `aisle_shelf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aisle_shelf` (
+  `id` int(11) NOT NULL,
+  `aisle_name` varchar(255) NOT NULL,
+  `shelf_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-(
+--
+-- Dumping data for table `aisle_shelf`
+--
 
-    item_id long,
+LOCK TABLES `aisle_shelf` WRITE;
+/*!40000 ALTER TABLE `aisle_shelf` DISABLE KEYS */;
+INSERT INTO `aisle_shelf` VALUES (1,'A2','2');
+/*!40000 ALTER TABLE `aisle_shelf` ENABLE KEYS */;
+UNLOCK TABLES;
 
-    store_id long,
+--
+-- Table structure for table `item_info`
+--
 
-    location_id long
+DROP TABLE IF EXISTS `item_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_info` (
+  `item_id` mediumtext NOT NULL,
+  `store_id` mediumtext NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `brand` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-);
+--
+-- Dumping data for table `item_info`
+--
 
+LOCK TABLES `item_info` WRITE;
+/*!40000 ALTER TABLE `item_info` DISABLE KEYS */;
+INSERT INTO `item_info` VALUES ('1','1','McClure\'s Garlic Dill Pickles',15.99,12,'McClure'),('2','1','Toothpaste',5.99,134,'Coalgate');
+/*!40000 ALTER TABLE `item_info` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `item_location`
+--
 
-create table location
+DROP TABLE IF EXISTS `item_location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item_location` (
+  `item_id` mediumtext,
+  `store_id` mediumtext,
+  `location_id` mediumtext
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-(
+--
+-- Dumping data for table `item_location`
+--
 
-    id long,
+LOCK TABLES `item_location` WRITE;
+/*!40000 ALTER TABLE `item_location` DISABLE KEYS */;
+INSERT INTO `item_location` VALUES ('1','1','1'),('2','1','1');
+/*!40000 ALTER TABLE `item_location` ENABLE KEYS */;
+UNLOCK TABLES;
 
-    section_id int,
+--
+-- Table structure for table `location`
+--
 
-    aisle_shelf_id int
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `location` (
+  `id` mediumtext,
+  `section_id` int(11) DEFAULT NULL,
+  `aisle_shelf_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-);
+--
+-- Dumping data for table `location`
+--
 
+LOCK TABLES `location` WRITE;
+/*!40000 ALTER TABLE `location` DISABLE KEYS */;
+INSERT INTO `location` VALUES ('1',1,1);
+/*!40000 ALTER TABLE `location` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `section`
+--
 
-create table section
+DROP TABLE IF EXISTS `section`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `section` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-(
+--
+-- Dumping data for table `section`
+--
 
-    id int not null,
+LOCK TABLES `section` WRITE;
+/*!40000 ALTER TABLE `section` DISABLE KEYS */;
+INSERT INTO `section` VALUES (1,'Pickles');
+/*!40000 ALTER TABLE `section` ENABLE KEYS */;
+UNLOCK TABLES;
 
-    name varchar(255) not null
+--
+-- Table structure for table `store`
+--
 
-);
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store` (
+  `id` mediumtext NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(1000) NOT NULL,
+  `zip` int(11) NOT NULL,
+  `city` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `store`
+--
 
+LOCK TABLES `store` WRITE;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+INSERT INTO `store` VALUES ('1','Fresh Madison Market','703 University Avenue',53715,'Madison');
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-create table aisle_shelf
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-(
-
-    id int not null,
-
-    aisle_name varchar(255) not null,
-
-    shelf_name varchar(255) not null
-
-);
-
-
-
-create table store
-
-(
-
-    id long not null,
-
-    name varchar(255) not null,
-
-    address varchar(1000) not null,
-
-    zip int not null,
-
-    city varchar(255) not null
-
-);
-
-
-
-create table item_info
-
-(
-
-    item_id long not null,
-
-    store_id long not null,
-
-    name varchar(255) not null,
-
-    price float not null,
-
-    quantity int not null,
-
-    brand varchar(255)
-
-);
-
-
-
--- Populate data here
-
-insert into store values (1,"Fresh Madison Market", "703 University Avenue", 53715, "Madison");
-
-insert into item_info values(1,1,"McClure's Garlic Dill Pickles", 15.99, 12, "McClure");
-
-insert into item_location values(1,1,1);
-
-insert into location values(1,1,1);
-
-insert into section values(1, "Pickles");
-
-insert into aisle_shelf values(1, "A2", "2");
+-- Dump completed on 2014-03-31  1:09:19
